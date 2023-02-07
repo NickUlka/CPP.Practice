593ce27d364f44ebfc4d09c6d3de18b2a6cdce24
NickUlka <russkaya2011@gmail.com>
2023-02-08 01:09:08 +0300

    Practice 4

---
+++ Practice 4.1/Practice 4.1.cpp
@@ -0,0 +1,46 @@
+﻿
+//Вычисление корней квадратного уравнения
+
+#include <iostream>
+#include <cmath>
+
+int Myroot(double, double, double, double&, double&);
+
+
+int main()
+{
+    double x1 = 0, x2 = 0;
+    //double a = 1, b = 2, c = 3; // нет корней
+    double a = 2, b = 4, c = 2; // одинаковые корни
+   // double a = 1, b = 4, c = 3; // 2 корня
+    int result = Myroot(a, b, c, x1, x2);
+    switch (result)
+    {
+    case 1:
+        std::cout << "«The roots of the equation are: x1 = " << x1 << ", x2 = " << x2 << std::endl;
+        break;
+    case 0:
+        std::cout << "The roots of the equation are equal: x1 = x2 = " << x1 << std::endl;
+        break;
+    default:
+        std::cout << "There are no roots in the equation";
+    }
+
+}
+
+int Myroot(double a, double b, double c, double& x1, double& x2) {
+    double d = b * b - 4 * a * c;
+   
+    if (d > 0) {
+        x1 = (-b - sqrt(d)) / 2 * a;
+        x2 = (-b + sqrt(d)) / 2 * a;
+        return 1;
+    }
+    else if (d == 0) {
+        x1 = x2 = -b / 2 * a;
+        return 0;
+    }
+    else
+        return -1;
+
+}
---
+++ Practice4.2/Practice4.2.cpp
@@ -0,0 +1,32 @@
+﻿
+// Реализация ввода данных
+
+
+#include <iostream>
+using namespace std;
+
+bool Input (int &a, int &b){
+    cout << "Input 2  numbers: \n";
+    cin >> a >> b;
+    if (a!=b) 
+        return true;
+    else
+        return false;   
+}
+
+
+int main()
+{
+    int a, b;
+    if (Input(a, b) == false) // if(!Input(a,b))
+    {
+        cerr << "error";
+        return 1;
+    }
+    int s = a + b;
+    cout << "The sum of this 2 numbers is "<<s<<endl;
+        return 0;
+
+
+}
+

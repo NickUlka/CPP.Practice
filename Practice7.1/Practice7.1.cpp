f252c32fff74c9102ab69fba4aca4eb140bbee19
NickUlka <russkaya2011@gmail.com>
2023-02-16 17:34:57 +0300

    changed 7.1

--- Practice7.1/Practice7.1.cpp
+++ Practice7.1/Practice7.1.cpp
@@ -9,7 +9,7 @@ struct Time
     int hours;
     int minutes;
     int seconds;
-    void plusInterval(Time t) {
+   Time plusInterval(Time t) {
         Time time;
         time.hours = hours + t.hours;
         time.minutes = minutes + t.minutes;
@@ -23,9 +23,10 @@ struct Time
             time.hours++;
             time.minutes -= 60;
         }
-        cout << "The sum of two times is " << time.hours << ":" << time.minutes << ":" << time.seconds << endl;
+        return time; 
+
     }
-    void minusInterval(Time t, long secTime1) {
+    Time minusInterval(Time t, long secTime1) {
         Time time;
         long secTime2 = t.seconds + 60 * (t.minutes + t.hours * 60);
         long dif = secTime1 - secTime2;
@@ -36,7 +37,7 @@ struct Time
         time.minutes = dif % 3600 / 60;
         time.seconds= dif % 3600 % 60;
   
-        cout << "The interval between two times is " << time.hours << ":" << time.minutes << ":" << time.seconds << endl;
+        return time;
     }
 };
 
@@ -53,14 +54,16 @@ Time inputTime() {
 
 int main()
 {
-    Time time1, time2;
+    Time time1, time2, time3;
     cout << "Input time to calculate in seconds";
     time1 = inputTime();
     long secTime = time1.seconds + 60 *(time1.minutes + time1.hours * 60);
     cout << "Your time in seconds is equal to "<<secTime<<endl;
     cout << "Input time to calculate the interval between first and second time";
     time2 = inputTime();
-    time1.plusInterval(time2);
-    time1.minusInterval(time2, secTime);
+    time3=time1.plusInterval(time2);
+    cout << "The sum of two times is " << time3.hours << ":" << time3.minutes << ":" << time3.seconds << endl;
+    time3=time1.minusInterval(time2, secTime);
+    cout << "The interval between two times is " << time3.hours << ":" << time3.minutes << ":" << time3.seconds << endl;
 }
 
--- Practice8.1/Practice8.1.cpp
+++ Practice8.1/Practice8.1.cpp
@@ -15,7 +15,6 @@ public:
     Time() :hours(0), minutes(0), seconds(0)
     {
     }
-
     Time (int h, int m, int s): hours(h), minutes(m), seconds(s)
     {
         if (seconds/60>0) {

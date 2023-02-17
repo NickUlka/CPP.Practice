8ee4405f905a43853db3cb78d17486137cae3e19
NickUlka <russkaya2011@gmail.com>
2023-02-17 20:05:04 +0300

    check time method is added

--- Practice8.1/Practice8.1.cpp
+++ Practice8.1/Practice8.1.cpp
@@ -10,39 +10,36 @@ private:
     int hours;
     int minutes;
     int seconds;
+    void CheckTime(int& h,int& m, int& s)
+    {
+        {
+            if (s / 60 > 0) {
+                m += s / 60;
+                s = s % 60;
+            }
+            if (m / 60 > 0) {
+                h += m / 60;
+                m = m % 60;
+            }
+        }
+    }
 
 public:
     Time() :hours(0), minutes(0), seconds(0)
-    {
-    }
+    {}
     Time (int h, int m, int s): hours(h), minutes(m), seconds(s)
     {
-        if (seconds/60>0) {
-            minutes+=seconds/60;
-            seconds =seconds%60;
-        }
-        if (minutes / 60 > 0) {
-            hours += minutes / 60;
-            minutes = minutes % 60;
-        }
+        CheckTime(h, m, s);
     }
    
     friend void showTime(Time&); 
    
-    Time plusTime(const Time& t) const
-    {
+    Time plusTime(const Time& t) const{
         Time time;
         time.hours = hours + t.hours;
         time.minutes = minutes + t.minutes;
         time.seconds = seconds + t.seconds;
-        if (time.seconds / 60 > 0) {
-            time.minutes += time.seconds / 60;
-            time.seconds = time.seconds % 60;
-        }
-        if (time.minutes / 60 > 0) {
-            time.hours += time.minutes / 60;
-            time.minutes = time.minutes % 60;
-        }
+        time.CheckTime(time.hours, time.minutes, time.seconds);
         return time;
     }
 };

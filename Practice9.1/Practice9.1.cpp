98d0c81505aed75cdf9141c7a4a8e54da44e952d
NickUlka <russkaya2011@gmail.com>
2023-02-23 02:43:40 +0300

    9 and 10 control tasks

---
+++ Practice9.1/Practice9.1.cpp
@@ -0,0 +1,35 @@
+﻿//  Безопасная реализация класса Triangle
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+
+double triangleSquare(double a, double b, double c) {
+    if (a > b + c || b > a + c || c > b + a)
+        throw exception("Incorrect side length - one side should not be longer than the sum of lengths of two other sides");
+        double p = (a + b + c) / 2;
+        double square = sqrt(p * (p - a) * (p - b) * (p - c));
+    return square;
+}
+
+int main()
+{
+
+        std::cout << "Input a dimension of 3 sides of a triangle: ";
+        double side1, side2, side3;
+        while (std::cin >> side1 >> side2 >> side3)
+        {
+        try {
+            std::cout << "The square of a triangle is " << triangleSquare(side1, side2, side3) << std::endl;
+            break;
+        }
+        catch (const exception& err) {
+            std::cout << err.what()<<"\nInput a correct dimension of 3 sides of a triangle: ";
+        }
+        }
+        return 0;
+}
---
+++ Practice9.2/Practice9.2.cpp
@@ -0,0 +1,113 @@
+﻿//  Безопасная реализация класса Time
+
+
+#include <iostream>
+using namespace std;
+
+
+    class Time {
+
+    private:
+        int hours;
+        int minutes;
+        int seconds;
+        void CheckTime(int& h, int& m, int& s)
+        {
+            {
+                if (s / 60 > 0) {
+                    m += s / 60;
+                    s = s % 60;
+                }
+                if (m / 60 > 0) {
+                    h += m / 60;
+                    m = m % 60;
+                }
+            }
+        }
+
+    public:
+
+        class WrongTime
+        {
+        public:
+            WrongTime(int err) 
+            {
+                switch (err)
+                {
+                case 1:
+                    message = "Hour time should be less than 24";
+                    break;
+                case 2:
+                    message = "The time can't be negative";
+                    break;
+                case 3:
+                    message = "It is already another day";
+                }
+              
+            }
+            void printMessage() const
+            {
+                cout << message << endl;
+            }
+        private:
+            string message;
+        };
+        Time() :hours(0), minutes(0), seconds(0)
+        {}
+        Time(int h, int m, int s) : hours(h), minutes(m), seconds(s)
+        {
+           
+            if (h >24)
+                throw WrongTime(1);
+            if (h < 0 || m < 0 || s < 0)
+                throw WrongTime(2);
+            CheckTime(h, m, s);
+        
+        }
+
+        friend void showTime(Time&);
+
+        Time plusTime(const Time& t) const {
+            Time time;
+            time.hours = hours + t.hours;
+            time.minutes = minutes + t.minutes;
+            time.seconds = seconds + t.seconds;
+            time.CheckTime(time.hours, time.minutes, time.seconds);
+            if (time.hours > 24)
+                throw WrongTime(3);
+            return time;
+        }
+    };
+
+    void showTime(Time & time)
+    {
+        cout << time.hours << ":" << time.minutes << ":" << time.seconds << endl;
+    }
+
+
+    int main()
+    {
+        int h, m, s;
+        cout << "Input time in hours, minutes and seconds:\n";
+            cin >> h >> m >> s;
+            
+            try {
+                Time time1(h, m, s);
+                cout << "Input another time in hours, minutes and seconds:\n";
+                cin >> h >> m >> s;
+                Time time2(h, m, s);
+                Time time3 = time1.plusTime(time2);
+                showTime(time3);
+            }
+        
+
+            catch (Time::WrongTime& err)
+            {
+                err.printMessage();     
+            }
+    }
+
+       
+    
+
+

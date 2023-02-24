93824e17c7b00fd25a01fdc6b20169e8571f30eb
NickUlka <russkaya2011@gmail.com>
2023-02-24 19:40:20 +0300

    practice 11

---
+++ Practice 11.1/Practice 11.1.cpp
@@ -0,0 +1,129 @@
+﻿//  Перегрузка операторов в классе Time
+
+#include <iostream>
+using namespace std;
+
+class Time {
+
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+    void CheckTime(int& h, int& m, int& s)
+    {
+        {
+            if (s / 60 > 0) {
+                m += s / 60;
+                s = s % 60;
+            }
+            if (m / 60 > 0) {
+                h += m / 60;
+                m = m % 60;
+            }
+        }
+    }
+
+
+
+public:
+   
+    Time() :hours(0), minutes(0), seconds(0)
+    {}
+    Time(int h, int m, int s) : hours(h), minutes(m), seconds(s)
+    {
+        CheckTime(h, m, s);   
+    }
+    Time(float time) {
+        hours = (int)time;
+        float floatMin = 60*(time - hours);
+         minutes = (int)floatMin;
+        seconds = (int)(60 * (floatMin - minutes));
+    }
+
+    //
+
+    long secConvert() const {
+        return   seconds + 60 * (minutes + hours * 60);
+    }
+
+    friend void showTime(Time&);
+
+    Time operator+ (const Time&) const;
+    Time operator- (const Time&) const;
+    bool operator>= (const Time&) const;
+
+};
+
+Time Time::operator+ (const Time& t) const
+{
+    Time time;
+    time.hours = hours + t.hours;
+    time.minutes = minutes + t.minutes;
+    time.seconds = seconds + t.seconds;
+    time.CheckTime(time.hours, time.minutes, time.seconds);
+    return time;
+  
+}
+
+Time Time::operator- (const Time& t) const
+{
+    Time time;  
+        long dif = secConvert() - t.secConvert();
+        time.hours = dif / 3600;
+        time.minutes = dif % 3600 / 60;
+        time.seconds = dif % 3600 % 60;
+        return time;
+}
+
+bool Time::operator>= (const Time& t) const
+{
+    bool flag = 1;
+    if (secConvert() >= t.secConvert())
+        flag;
+    else
+        flag = 0;
+    return flag;
+}
+
+
+void showTime(Time& time)
+{
+    cout << time.hours << ":" << time.minutes << ":" << time.seconds << endl;
+}
+
+int main()
+{
+    int h, m, s;
+    cout << "Input time in hours, minutes and seconds:\n";
+    cin >> h >> m >> s;
+    Time time1(h, m, s);
+    cout << "Input another time in hours, minutes and seconds:\n";
+    cin >> h >> m >> s;
+    Time time2(h, m, s);
+    Time time3;
+    time3 = time1 + time2;
+    cout << "The sum of these two times is ";
+    showTime(time3);
+    if (time1 >= time2) {
+        time3 = time1 - time2;
+        cout << "The substitution of these two times is ";
+        showTime(time3);
+    }
+    else
+        cout << "Fist time is less than second one. We can't subtract because of negative result!" << endl;
+    cout << "Input time in float format: ";
+    float floatTime;
+    cin >> floatTime;
+    cout << "The sum of "<< floatTime<< " hour and ";
+    showTime(time2);
+    Time time4(floatTime);
+    time3 = time2 + time4;
+    showTime(time3);
+    time3 = time4 + time2;
+    cout << "And sum vice-versa ";
+    showTime(time3);
+    return 0;
+}
+
+
---
+++ Practice11.2/Practice11.2.cpp
@@ -0,0 +1,56 @@
+﻿// Реализация сортировки точек в векторе
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> // для алгоритма сортировки
+#include <cmath>
+
+class Point {
+private:
+	double x;
+	double y;
+	double distanceToCentre() const {
+		return sqrt(x * x + y * y);
+	}
+
+public:
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+
+	
+
+	friend std::ostream& operator<< (std::ostream&, const Point&);
+	friend bool operator<(const Point&, const Point&);
+};
+
+bool operator<(const Point& p1, const Point& p2) {
+	if (p1.distanceToCentre() < p2.distanceToCentre())
+		return true;
+	else
+		return false;
+}
+
+ std::ostream& operator<< (std::ostream& out, const Point& p) {
+	 out << "Point(" << p.x << ", " << p.y << ")";
+	 return out;
+}
+
+
+int main()
+{
+	std::vector<Point> v;
+	v.push_back(Point(1, 2));
+	v.push_back(Point(10, 12));
+	v.push_back(Point(21, 7));
+	v.push_back(Point(4, 8));
+	std::sort(v.begin(), v.end()); // перегрузка оператора < для
+	// класса Point
+	for (auto& point : v)
+		std::cout << point << '\n'; // перегрузка оператора << для  класса Point
+	return 0;
+}
+
